23f936a17f16b7260b39356e018cadf03408de53
3v01ut10n <arxonix7@gmail.com>
2020-09-22 18:08:28 +0400

    Уведомления о дне рождения за неделю

--- notify.py
+++ notify.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from database import db
 from config import group_id
 
@@ -7,16 +9,35 @@ current_date = db.get_current_date()
 active_birthdays = db.select_active_birthdays()
 
 # Поиск именинников за сегодня.
-birthday_boys = []
+today_birthday_boys = []
 for person in active_birthdays:
     if (person["date"].day == current_date.day) and (person["date"].month == current_date.month):
-        birthday_boys.append(person)
+        today_birthday_boys.append(person)
 
 # Если именинник есть, проведётся отправка.
-for person in birthday_boys:
+for person in today_birthday_boys:
     db.bot.send_message(
         group_id,
         f"Сегодня день рождения отмечает {person['name']}!\n"
+        f"Ему исполняется {current_date.year - person['date'].year}.\n"
+        f"Номер телефона: {person['telephone']}"
+    )
+
+
+# Поиск именинников за неделю.
+week_birthday_boys = []
+for person in active_birthdays:
+    date_birthday_in_current_year = date(current_date.year, person["date"].month, person["date"].day)
+    delta = (date_birthday_in_current_year - current_date).days
+    print(delta)
+    if delta == 7:
+        week_birthday_boys.append(person)
+
+# Если именинник есть, проведётся отправка.
+for person in week_birthday_boys:
+    db.bot.send_message(
+        group_id,
+        f"Через неделю день рождения у {person['name']}!\n"
         f"Ему исполняется {current_date.year - person['date'].year}.\n"
         f"Номер телефона: {person['telephone']}"
     )
